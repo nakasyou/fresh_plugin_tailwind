426b62dae2787c15583f0ec5b7500cfc44ba958c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-12 14:16:58 +0900

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,18 @@
+import { Plugin } from "$fresh/server.ts"
+
+import postcss from 'npm:postcss'
+import tailwindcss from 'npm:tailwindcss'
+
+const makecss = async () => (await postcss([tailwindcss]).process('@tailwind base; @tailwind components; @tailwind utilities;')).css
+
+export default async function pluginTailwind (): Plugin{
+  return {
+    name: 'tailwind',
+    async renderAsync(ctx) {
+      const cssText = makecss()
+      return [{
+        cssText,
+      }]
+    },
+  }
+}
